b7180c7978cb21bbf770ed95a0d951d5451a13d4
dapengchina <aoqishequ@163.com>
2019-06-13 17:07:25 +0800

    Change-Id: I2e4e956908507ff7c901a76eef5cfa444e8bd961

--- src/main/java/com/csi/sbs/investment/business/controller/atomic/VMutual.java
+++ src/main/java/com/csi/sbs/investment/business/controller/atomic/VMutual.java
@@ -44,7 +44,7 @@ public class VMutual {
 	@SuppressWarnings({"rawtypes" })
 	@RequestMapping(value = "/fundQuotation/{fundcode}", method = RequestMethod.GET)
 	@ResponseBody
-	@ApiOperation(value = "This API is designed to return fundQuotation.", notes = "version 0.0.1")
+	@ApiOperation(value = "This API is designed to return the fund quotation.", notes = "version 0.0.1")
 	@ApiResponses({ @ApiResponse(code = 200, message = "Query completed successfully.(Returned By Get)"),
 			@ApiResponse(code = 404, message = "The requested deposit account does not exist.Action: Please make sure the account number and account type you’re inputting are correct."),
 			@ApiResponse(code = 201, message = "Normal execution. The request has succeeded. (Returned By Post)"),
